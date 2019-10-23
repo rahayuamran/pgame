bd7b103dc37e23b1eca3875ccaf40c6158c53c89
rahayuamran <56920983+rahayuamran@users.noreply.github.com>
2019-10-23 19:49:38 +0700

    code-tugas3

---
+++ pgame-16053/code-tugas3.py
@@ -0,0 +1,11 @@
+#latihan git push dan full from terminal
+
+#latihan buat variabel;
+nama = "nama saya : SRI RAHAYU AMRAN"
+print (nama)
+npm = "NPM : 121055520116053"
+print (npm)
+semester = "semester : 7(tujuh)"
+print (semester)
+matakuliah = "Mata Kuliah : Pemograman Game"
+print (matakuliah)
