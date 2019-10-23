dc5e0f8662544cfb98f657eba1598618bbdee3ff
rahayuamran <56920983+rahayuamran@users.noreply.github.com>
2019-10-23 19:32:49 +0700

    code_tugas2

---
+++ pgame-16053/code-tugas2.py
@@ -0,0 +1,3 @@
+print("hallo my name is SRI RAHAYU AMRAN you can call me AYU") #hallo nama saya SRI RAHAYU AMRAN, kamu bisa panggil saya AYU
+print("Saya Mahasiswa Teknik Informatika semester 7 di UMMU")
+print(" Saya orang MAKIAN") #baris ini tidak di eksekusi
