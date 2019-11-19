f76671c0202e013aab6673d392d0b0b696dace44
rahayuamran <56920983+rahayuamran@users.noreply.github.com>
2019-11-19 19:57:23 +0700

    pratikum 5

---
+++ pgame-16053/code-54.py
@@ -0,0 +1,18 @@
+# Pemrograman Game Praktikum 5
+# Latihan code 5.4 : Sets
+
+# Buat variabel set dan memberikan nilai elemen
+buah = { 'Durian', 'Mangga', 'Salak', 'Mangga','Salak',
+	'Durian' }
+
+# print set
+print ("Element Set :", buah)
+
+# menambah satu elemen ke dalam set
+buah.add ("Melon")
+
+# menambah lebih dari satu elemen ke set
+buah.update ( ["Pepaya", "Semangka"])
+
+# print set setelah perubahan penambahan
+print ("Element Set :", buah)
