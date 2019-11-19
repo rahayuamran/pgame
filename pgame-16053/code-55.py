fe014f70c439215cb906feab07d260503a916676
rahayuamran <56920983+rahayuamran@users.noreply.github.com>
2019-11-19 19:58:54 +0700

    pratikum 5

---
+++ pgame-16053/code-55.py
@@ -0,0 +1,27 @@
+# Pemrograman Game Praktikum 5
+# Latihan code 5.5 : Kondisional
+
+# Buat variabel A dan B
+A = 50
+B = 20
+
+# menggunakan kondisionak if
+if A > B:
+	print ("A lebih besar dari B")
+# setelah tanda ":" meruupakan awal blok,
+# penulisan harus masuk beberapa spasi
+# atau menggunakan tab
+
+# menggunakan kondisionak if, elif
+if A > B :
+	print ("A lebih besar dari B")
+elif A== B:
+	print ("A sama dengan B")
+
+# menggunakan kondisionak if, elif, else
+if A > B:
+	print ("A lebih besar dari B")
+elif A == B:
+	print ("A sama dengan B")
+else:
+	print ("A Lebih Kecil dari B")
