71724c9f2f1d7180cf9630486864585ca0c40884
rahayuamran <56920983+rahayuamran@users.noreply.github.com>
2019-11-19 20:01:05 +0700

    pratikum 5

---
+++ pgame-16053/Tugas-code6.py
@@ -0,0 +1,17 @@
+# Tugas_code6 : Menggunakan Kondisional
+
+nilai = 140
+if nilai > 80:		# menggunakan kondisional if
+    print ("Sangat Baik")
+
+nilai = 80
+if nilai > 65 :		# menggunakan kondisional if
+    print ("Cukup Baik")
+
+nilai = 65
+if nilai > 50 :		# menggunakan kondisional if
+    print ("Baik")
+
+nilai = 20
+if nilai > 15 :		# menggunakan kondisional if
+    print ("Kurang")
