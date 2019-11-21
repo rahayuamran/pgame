ba6ab6d3ca0a9e469b867cacd76cf84c383307db
rahayuamran <56920983+rahayuamran@users.noreply.github.com>
2019-11-21 15:35:01 +0700

    code-tugas8.py

---
+++ pgame-16053/code-tugas8.py
@@ -0,0 +1,18 @@
+#Operator Penugasan
+
+x = 15 # Buat variabel x dan isi dengan nilai 15
+print (x)
+x += 15 # Tambahkan nilai 15 ke variabel x
+print (x)
+x -= 15
+print (x)
+x *= 20
+print (x)
+x /= 30
+print (x)
+x %= 15
+print (x)
+x **= 25
+print (x)
+x //= 50
+print (x)
