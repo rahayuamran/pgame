f7a2571eae3d5c90276b872e07a0492e2cb9ba2b
rahayuamran <56920983+rahayuamran@users.noreply.github.com>
2019-11-21 15:32:47 +0700

    code-tugas7.py

---
+++ pgame-16053/code-tugas7.py
@@ -0,0 +1,20 @@
+#Operator Logical
+
+True and True # Operator and
+True and False # Operator and
+True or False # Operator or
+False or True # Operator or
+False or False # Operator or
+True or True # Operator or
+not True # Operator not
+not False # Operator not
+
+#Latihan Menggunakan Perintah Print
+print ('True and True')
+print ('True and False')
+print ('True or False')
+print ('False or True')
+print ('False or False')
+print ('True or True')
+print ('not True')
+print ('not False')
