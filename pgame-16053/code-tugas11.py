a68d54ca70e7fa2644ce34d3a4899dd7bbd3602c
rahayuamran <56920983+rahayuamran@users.noreply.github.com>
2019-11-21 15:39:59 +0700

    code-tugas11.py

---
+++ pgame-16053/code-tugas11.py
@@ -0,0 +1,13 @@
+#Operator Identitas
+
+a,b = 55,44
+a & b # Melakukan operasi and pada biner a dan b
+a | b # Melakukan operasi or pada biner a dan b
+a,b = 55,66
+a ^ b
+a,b = 55,66
+~a
+a,b = 55,66
+a << 2
+a,b = 55,66
+a >> 2
